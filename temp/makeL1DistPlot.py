3cbb05da873c853dfa94157eb1e46ef813716b10
ekauffma <elmaka8700@gmail.com>
2026-03-01 03:24:12 +0000

    figure C init

---
+++ temp/makeL1DistPlot.py
@@ -0,0 +1,160 @@
+import argparse
+from coffea.util import load
+from hist.tag import rebin as reb
+import matplotlib.pyplot as plt
+import mplhep as hep
+import numpy as np
+import os
+import yaml
+
+hep.style.use('CMS')
+
+# Default axis limits and x-labels per object type
+OBS_DEFAULTS = {
+    "ht": {
+        "rebin": 5,
+        "hist_key": "l1_ht",
+        "x_label": r"L1 $H_T$ [GeV]",
+        "x_min": 0, "x_max": 1000,
+        "y_min": 1e0, "y_max": 5e9,
+    },
+    "met": {
+        "rebin": 1,
+        "hist_key": "l1_met",
+        "x_label": r"L1 $p_T^{\text{miss}}$ [GeV]",
+        "x_min": 0, "x_max": 180,
+        "y_min": 1e0, "y_max": 5e9,
+    },
+}
+
+TRIGGER_LABELS = {
+    'DST_PFScouting_ZeroBias' : 'Zero Bias',
+    'DST_PFScouting_ZeroBias_DST_PFScouting_AXONominal': 'Zero Bias & AXO Medium',
+    'DST_PFScouting_ZeroBias_DST_PFScouting_CICADAMedium': 'Zero Bias & CICADA Medium'
+}
+
+NORM = False
+
+def draw_hist1d(hist_in=None, ax=None, label="", rebin=1,
+                obj=None, norm=False, gg=None, linestyle='solid', color=None):
+
+    hist_in = hist_in[reb(rebin)]
+
+    hist_data = hist_in.to_numpy()
+    ndim = len(hist_data)
+
+    if ndim == 2:
+        counts, bins = hist_data
+    else:
+        counts, _, bins = hist_data
+        counts = np.sum(counts, axis=0)
+
+    if len(counts) > 0:
+        norm_factor = np.sum(counts) * np.diff(bins) if norm else 1
+        _counts = counts / norm_factor if norm else counts
+        errs = np.sqrt(counts) / norm_factor if norm else np.sqrt(counts)
+        _errs = np.where(_counts == 0, 0, errs)
+
+        bin_centres = 0.5 * (bins[1:] + bins[:-1])
+
+        if color is not None:
+            l = ax.errorbar(x=bin_centres, y=_counts, yerr=_errs, linestyle="", color=color)
+        else:
+            l = ax.errorbar(x=bin_centres, y=_counts, yerr=_errs, linestyle="")
+        color = l[0].get_color()
+        ax.errorbar(
+            x=bins, y=np.append(_counts, _counts[-1]), drawstyle="steps-post", label=label,
+            color=color, linestyle=linestyle
+        )
+    else:
+        if color is not None:
+            l = ax.errorbar(x=[], y=[], yerr=[], drawstyle="steps-post", color=color)
+        else:
+            l = ax.errorbar(x=[], y=[], yerr=[], drawstyle="steps-post")
+        color = l[0].get_color()
+        ax.errorbar(x=[], y=[], drawstyle="steps-post", label=label, color=color, linestyle=linestyle)
+
+    return l
+
+def main(args):
+
+    hist_result = load(args.input)
+    print(hist_result)
+
+    defaults = OBS_DEFAULTS[args.observable]
+
+    x_min = args.x_min if args.x_min is not None else defaults["x_min"]
+    x_max = args.x_max if args.x_max is not None else defaults["x_max"]
+    y_min = args.y_min if args.y_min is not None else defaults["y_min"]
+    y_max = args.y_max if args.y_max is not None else defaults["y_max"]
+
+    fig, ax = plt.subplots(figsize=(9, 7))
+
+    rebin = defaults["rebin"]
+
+    triggers = [
+        "DST_PFScouting_ZeroBias",
+        "DST_PFScouting_ZeroBias_DST_PFScouting_AXONominal",
+        "DST_PFScouting_ZeroBias_DST_PFScouting_CICADAMedium"
+    ]
+
+    for trigger in triggers:
+        draw_hist1d(
+            hist_in=hist_result["hists"]["l1_ht"]["2024I", trigger, :],
+            ax=ax,
+            label=TRIGGER_LABELS[trigger],
+            rebin=rebin,
+            norm=NORM,
+        )
+
+    ax.set_yscale("log")
+    ax.set_xlim([x_min, x_max])
+    ax.set_ylim([y_min, y_max])
+    ax.legend(loc="upper right", frameon=False, fontsize=16)
+    ax.set_ylabel(f"Events{' [A.U.]' if NORM else ''}", loc="top", fontsize=25)
+    ax.set_xlabel(defaults["x_label"], fontsize=25)
+    
+    hep.cms.label(
+        "Preliminary",
+        data=True,
+        lumi=11.45,
+        year="2024",
+        com=13.6,
+        fontsize=18,
+    )
+
+    out_dir = os.path.dirname(args.output)
+    if out_dir:
+        os.makedirs(out_dir, exist_ok=True)
+    fig.savefig(f"{args.output}.pdf", format="pdf", bbox_inches="tight")
+    fig.savefig(f"{args.output}.png", format="png", bbox_inches="tight")
+    print(f"Saved {args.output}.pdf and {args.output}.png")
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Make a plot of L1 HT or missing pT of AXO and CICADA out of Zero Bias events"
+    )
+    parser.add_argument(
+        "--observable",
+        required=True,
+        choices=["ht", "met"],
+        help="Which observable to plot"
+    )
+    parser.add_argument(
+        "--input",
+        default="histograms/hist_result_plotD_plotE.pkl",
+        help="Input .pkl histogram file"
+    )
+    parser.add_argument(
+        "--output",
+        required=True,
+        help="Full output path prefix, e.g. plots/l1_ht_efficiency (extensions .pdf/.png added automatically)"
+    )
+    parser.add_argument("--x-min", type=float, default=None, help="x-axis minimum")
+    parser.add_argument("--x-max", type=float, default=None, help="x-axis maximum")
+    parser.add_argument("--y-min", type=float, default=None, help="y-axis minimum")
+    parser.add_argument("--y-max", type=float, default=None, help="y-axis maximum")
+
+    args = parser.parse_args()
+    main(args)
---
+++ temp/makeObjMultPlots.py
@@ -0,0 +1,265 @@
+import argparse
+from coffea.util import load
+from hist.tag import rebin as reb
+import matplotlib.pyplot as plt
+import numpy as np
+import mplhep as hep
+import os
+import yaml
+
+hep.style.use('CMS')
+
+triggers = [
+    "DST_PFScouting_ZeroBias",
+    "DST_PFScouting_JetHT",
+    "DST_PFScouting_CICADAMedium",
+    "DST_PFScouting_DoubleMuon",
+    "DST_PFScouting_AXONominal",
+]
+
+# Default axis limits and x-labels per object type
+OBJ_DEFAULTS = {
+    "L1Mu": {
+        "hist_key": "L1Mu_mult",
+        "x_label": r"$N_{\text{L1Mu}}$",
+        "x_min": -0.5, "x_max": 8.5,
+        "y_min": 5e-10, "y_max": 1e2,
+    },
+    "L1EG": {
+        "hist_key": "L1EG_mult",
+        "x_label": r"$N_{\text{L1EG}}$",
+        "x_min": -0.5, "x_max": 12.5,
+        "y_min": 5e-5, "y_max": 5e1,
+    },
+    "L1Jet": {
+        "hist_key": "L1Jet_mult",
+        "x_label": r"$N_{\text{L1Jet}}$",
+        "x_min": -0.5, "x_max": 12.5,
+        "y_min": 5e-8, "y_max": 5e2,
+    },
+}
+
+
+def draw_hist1d(plotting_config, hist_in=None, ax=None, label="", rebin=1,
+                obj=None, norm=False, gg=None, linestyle='solid', color=None):
+
+    hist_in = hist_in[reb(rebin)]
+
+    hist_data = hist_in.to_numpy()
+    ndim = len(hist_data)
+
+    if ndim == 2:
+        counts, bins = hist_data
+    else:
+        counts, _, bins = hist_data
+        counts = np.sum(counts, axis=0)
+
+    if len(counts) > 0:
+        norm_factor = np.sum(counts) * np.diff(bins) if norm else 1
+        _counts = counts / norm_factor if norm else counts
+        errs = np.sqrt(counts) / norm_factor if norm else np.sqrt(counts)
+        _errs = np.where(_counts == 0, 0, errs)
+
+        bin_centres = 0.5 * (bins[1:] + bins[:-1])
+
+        if color is not None:
+            l = ax.errorbar(x=bin_centres, y=_counts, yerr=_errs, linestyle="", color=color)
+        else:
+            l = ax.errorbar(x=bin_centres, y=_counts, yerr=_errs, linestyle="")
+        color = l[0].get_color()
+        ax.errorbar(
+            x=bins, y=np.append(_counts, _counts[-1]), drawstyle="steps-post", label=label,
+            color=color, linestyle=linestyle
+        )
+    else:
+        if color is not None:
+            l = ax.errorbar(x=[], y=[], yerr=[], drawstyle="steps-post", color=color)
+        else:
+            l = ax.errorbar(x=[], y=[], yerr=[], drawstyle="steps-post")
+        color = l[0].get_color()
+        ax.errorbar(x=[], y=[], drawstyle="steps-post", label=label, color=color, linestyle=linestyle)
+
+    return l
+
+
+def draw_ratio(plotting_config, hist_num, hist_denom, ax=None, color=None,
+               label="", rebin=1, norm=False, gg=None):
+
+    hist_num = hist_num[reb(rebin)]
+    hist_denom = hist_denom[reb(rebin)]
+
+    data_num = hist_num.to_numpy()
+    data_denom = hist_denom.to_numpy()
+
+    if len(data_denom) == 2:
+        counts_denom, bins = data_denom
+        counts_num, _ = data_num
+    else:
+        counts_denom, _, bins = data_denom
+        counts_num, _, _ = data_num
+        counts_denom = np.sum(counts_denom, axis=0)
+        counts_num = np.sum(counts_num, axis=0)
+
+    norm_factor_denom = np.sum(counts_denom) * np.diff(bins) if norm else 1
+    counts_denom = counts_denom / norm_factor_denom if norm else counts_denom
+    errs_denom = np.sqrt(counts_denom * (1 - counts_denom / norm_factor_denom)) / norm_factor_denom if norm else np.sqrt(counts_denom)
+
+    norm_factor_num = np.sum(counts_num) * np.diff(bins) if norm else 1
+    counts_num = counts_num / norm_factor_num if norm else counts_num
+    errs_num = np.sqrt(counts_num * (1 - counts_num / norm_factor_num)) / norm_factor_num if norm else np.sqrt(counts_num)
+
+    denom = np.where(counts_denom == 0, np.nan, counts_denom)
+    ratio = counts_num / denom
+    x = 0.5 * (bins[:-1] + bins[1:])
+
+    error = ratio * np.sqrt((errs_num / counts_num)**2 + (errs_denom / counts_denom)**2)
+
+    if color is not None:
+        l = ax.errorbar(x=x, y=ratio, yerr=error, linestyle="", color=color)
+    else:
+        l = ax.errorbar(x=x, y=ratio, yerr=error, linestyle="")
+    color = l[0].get_color()
+    ax.errorbar(
+        x=bins, y=np.append(ratio, ratio[-1]), drawstyle="steps-post", label=label,
+        color=color, linestyle='solid'
+    )
+    return l
+
+
+def get_trigger_label(trigger, plotting_config):
+    try:
+        return plotting_config["trigger_labels"][trigger]
+    except:
+        print(trigger + " name not in plotting_config.yaml, please edit")
+        return trigger
+
+
+def make_plot(hist_result, thing_to_plot, triggers, x_label, plotting_config,
+              x_min, x_max, y_min, y_max, output,
+              rebin=1, log_scale=True, norm=False, leg_loc='upper right'):
+    print(thing_to_plot)
+
+    fig, ax = plt.subplots(2, figsize=(8, 8), sharex=True, gridspec_kw={'height_ratios': [2, 1]})
+    ax[1].plot(np.linspace(x_min, x_max, 10), np.ones(10), '--', color='darkgray')
+    hist_denom = hist_result['hists'][thing_to_plot][:, 'DST_PFScouting_ZeroBias', :].integrate("dataset")
+
+    for trigger in triggers:
+        print(trigger)
+        color = 'k' if trigger == "DST_PFScouting_ZeroBias" else None
+        trigger_label = get_trigger_label(trigger, plotting_config)
+        l = draw_hist1d(
+            plotting_config,
+            hist_in=hist_result['hists'][thing_to_plot][:, trigger, :].integrate("dataset"),
+            ax=ax[0],
+            label=trigger_label,
+            rebin=rebin,
+            norm=norm,
+            color=color
+        )
+        if trigger != "DST_PFScouting_ZeroBias":
+            color = l[0].get_color()
+            hist_num = hist_result['hists'][thing_to_plot][:, trigger, :].integrate("dataset")
+            draw_ratio(
+                plotting_config,
+                hist_num,
+                hist_denom,
+                ax=ax[1],
+                label=trigger_label,
+                rebin=rebin,
+                norm=norm,
+                color=color
+            )
+
+    ax[0].set_xlim([x_min, x_max])
+    ax[0].set_ylim([y_min, y_max])
+
+    if log_scale:
+        ax[0].set_yscale("log")
+        ax[1].set_yscale("log")
+    ax[0].set_ylabel(f"Events{' [A.U.]' if norm else ''}", loc="top", fontsize=14)
+    ax[1].set_ylabel("Ratio to Zero Bias", loc="top", fontsize=14)
+    ax[1].set_xlabel(x_label, fontsize=14, loc="right")
+    ax[0].legend(loc=leg_loc, frameon=False, fontsize=12)
+
+    hep.cms.label(
+        plotting_config["cms_label"],
+        data=True,
+        lumi=11.45,
+        year="2024",
+        com=13.6,
+        fontsize=12,
+        ax=ax[0],
+    )
+
+    fig.subplots_adjust(hspace=0)
+
+    # output is used directly as the full path prefix
+    out_dir = os.path.dirname(output)
+    if out_dir:
+        os.makedirs(out_dir, exist_ok=True)
+
+    fig.savefig(f"{output}.pdf", format="pdf", bbox_inches="tight")
+    fig.savefig(f"{output}.png", format="png", bbox_inches="tight")
+    print(f"Saved {output}.pdf and {output}.png")
+
+
+def main(args):
+    with open(args.plotting_config, "r") as stream:
+        plotting_config = yaml.safe_load(stream)
+
+    hist_result = load(args.input)
+
+    defaults = OBJ_DEFAULTS[args.object]
+
+    x_min = args.x_min if args.x_min is not None else defaults["x_min"]
+    x_max = args.x_max if args.x_max is not None else defaults["x_max"]
+    y_min = args.y_min if args.y_min is not None else defaults["y_min"]
+    y_max = args.y_max if args.y_max is not None else defaults["y_max"]
+
+    make_plot(
+        hist_result,
+        defaults["hist_key"],
+        triggers,
+        defaults["x_label"],
+        plotting_config,
+        x_min, x_max, y_min, y_max,
+        args.output,
+        rebin=1,
+        log_scale=True,
+        norm=True,
+    )
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Make a multiplicity plot for a single L1 object type (L1Mu, L1EG, or L1Jet)."
+    )
+    parser.add_argument(
+        "--object",
+        required=True,
+        choices=["L1Mu", "L1EG", "L1Jet"],
+        help="Which object type to plot"
+    )
+    parser.add_argument(
+        "--input",
+        default="histograms/hist_result_plotA_plotB_plotC.pkl",
+        help="Input .pkl histogram file"
+    )
+    parser.add_argument(
+        "--output",
+        required=True,
+        help="Full output path prefix, e.g. plots/mult_L1Mu (extensions .pdf/.png added automatically)"
+    )
+    parser.add_argument(
+        "--plotting-config",
+        default="plotting_config.yaml",
+        help="yaml file containing plotting options"
+    )
+    parser.add_argument("--x-min", type=float, default=None, help="x-axis minimum")
+    parser.add_argument("--x-max", type=float, default=None, help="x-axis maximum")
+    parser.add_argument("--y-min", type=float, default=None, help="y-axis minimum")
+    parser.add_argument("--y-max", type=float, default=None, help="y-axis maximum")
+
+    args = parser.parse_args()
+    main(args)
